1b72dae87a4ba18da52a9da313f956cd57a40893
cat9563 <crysatl9563@gmail.com>
2018-11-14 18:07:00 -0500

    finished chapter 6

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from collection.models import Cats
 # Register your models here.
+admin.site.register(Cats)
+
+#set up automated slug creation 
+class CatsAdmin(admin.ModelAdmin):
+    model = Cats
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.unregister(Cats)
+admin.site.register(Cats, CatsAdmin)
